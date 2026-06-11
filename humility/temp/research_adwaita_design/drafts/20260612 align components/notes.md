7c1ace0272cac85f58633442a0b7f74711890a34
Fira <xoyage@gmail.com>
2026-06-12 01:00:02 +0800

    .

---
+++ humility/temp/research_adwaita_design/drafts/20260612 align components/notes.md
@@ -0,0 +1,3 @@
+i guess it's better not to let it revise colors.
+
+it might make it usable, but as i have not figured out the original definitive colors, it's better to do it later
--- humility/temp/research_adwaita_design/ref/baseui_docs/components/otp-field/types.ts
+++
@@ -1,7 +0,0 @@
-import { OTPFieldPreview } from '@base-ui/react/otp-field';
-import { createMultipleTypes } from 'docs/src/utils/createTypes';
-
-const { types, AdditionalTypes } = createMultipleTypes(import.meta.url, OTPFieldPreview);
-
-export const TypesOTPField = types;
-export const TypesOTPFieldAdditional = AdditionalTypes;
