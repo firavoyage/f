b495c2c2b795d600490648148fb9bf015c5b819b
Fira <xoyage@gmail.com>
2026-06-19 18:41:25 +0800

    .

--- humility/source/web/index copy.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client"
-import { App } from "./app"
-
-import 'the-new-css-reset/css/reset.css';
-
-createRoot(document.getElementById("root")!).render(<App />)
