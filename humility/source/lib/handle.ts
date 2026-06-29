918217abccf68e8fe2433784f7d370bb2bb0d961
Fira <xoyage@gmail.com>
2026-06-29 22:25:54 +0800

    .

--- humility/source/lib/handle.ts
+++ humility/source/lib/handle.ts
@@ -1,3 +1,16 @@
-export function handle() {
-  
+export function handle(fn) {
+  try {
+    const result = fn()
+    if (result instanceof Promise || typeof result?.then == 'function') {
+      return (result as Promise<Result<any>>)
+        // async ok
+        .then((data) => data)
+        // async err
+        .catch((e) => err(e));
+    }
+
+    return result
+  } catch (e) {
+    return err(e)
+  }
 }
---
+++ humility/source/lib/test/handle.ts
@@ -0,0 +1,12 @@
+import { handle } from 'lib/handle';
+
+log(handle(() => {
+  throw 1
+}))
+
+log(await handle(async () => {
+  await fetch()
+}))
+
+
+
