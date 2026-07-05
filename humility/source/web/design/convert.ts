953d4bec89e3118b2e3588704b0237981a3b2acd
Fira <xoyage@gmail.com>
2026-07-05 17:40:00 +0800

    .

--- humility/source/web/design/convert.ts
+++ humility/source/web/design/convert.ts
@@ -1,3 +1,4 @@
+import { existsSync } from "node:fs";
 import { parse } from "yaml";
 
 const design = {
@@ -34,17 +35,53 @@ export function convert(design) {
         modes[variant] = `[data-${mode}="${variant}"]`
       }
     }
-  } 
+  }
 
   const styles = {
 
   }
 
-  for (const  of array) {
-    
+  for (const selector of Object.values(modes)) {
+    styles[selector] = {}
   }
 
+  delete design.modes
+
+  const existing_keys = {}
   // recursive
+  function flatten(design, prefix = []) {
+    if (typeof design != 'object') {
+      existing_keys[prefix.join(".")] = true
+    } else if (has(design, 'value')) {
+      existing_keys[prefix.join(".")] = true
+    } else {
+      for (const [key, value] of Object.entries(design)) {
+        flatten(value, [...prefix, key])
+      }
+    } 
+  }
+  flatten(design)
+
+  function css_var(prefix) {
+    return `--${prefix.join('-')}`
+  }
+
+  function traverse(design, prefix = []) {
+    if (typeof design != 'object') {
+      if (typeof design == 'string' && has(existing_keys, design)) {
+        styles[modes.default][css_var(prefix)] = `var(--${design.replaceAll('.', '-')})`
+      } else {
+        
+      } 
+    } else if (has(design, 'value')) {
+      existing_keys[prefix.join(".")] = true
+    } else {
+      for (const [key, value] of Object.entries(design)) {
+        traverse(value, [...prefix, key])
+      }
+    } 
+  }
+  
 
   return {
     "[data-accent=\"green\"]": {
@@ -62,3 +99,4 @@ export function main(yaml: string) {
   // convert to css
 }
 
+convert(design)
