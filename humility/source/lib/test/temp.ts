047123b52cb3ef65d5218a6296f6c0c13f61bb85
Fira <xoyage@gmail.com>
2026-07-05 23:20:00 +0800

    .

---
+++ humility/source/lib/test/temp.ts
@@ -0,0 +1,20 @@
+const steps = [
+  [4, 0.5],
+  [12, 1],
+  [16, 2],
+  [64, 4],
+  [80, 8],
+  [96, 16],
+]
+
+let beg = 0
+
+for (const [end, step] of steps) {
+  for (const value of each(beg, end, step)) {
+    log(`${value}: ${value * 0.25}rem`)
+  }
+
+  beg = end
+}
+
+
