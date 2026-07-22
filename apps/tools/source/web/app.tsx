b6a4a3f450a044a32e17d416d42e14137e887a0b
Fira <xoyage@gmail.com>
2026-07-22 16:20:02 +0800

    .

--- apps/tools/source/web/app.tsx
+++ apps/tools/source/web/app.tsx
@@ -3,7 +3,7 @@ import { use_bind } from 'web/lib/keyboard.use';
 import { sync_theme } from 'web/lib/sync_theme';
 
 import 'web/design/utilitarian/utilitarian.css'
-import 'web/design/utilitarian/utilitarian.styl'
+// import 'web/design/utilitarian/utilitarian.styl'
 import 'web/app.styl'
 
 import { call } from "web/lib/call";
