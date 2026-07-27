1f2aa2796191e99575464217ebde161fcb6f4094
Fira <xoyage@gmail.com>
2026-07-28 04:20:04 +0800

    .

--- apps/tools/source/web/lib/keyboard.use.ts
+++ apps/tools/source/web/lib/keyboard.use.ts
@@ -1,6 +1,15 @@
 import { bind, unbind } from 'web/lib/keyboard';
 import { useEffect } from 'react';
 
+/**
+ * bind an action to a shortcut
+ * 
+ * multiple actions can bind to the same shortcut simultaneously
+ * 
+ * global: fire the shortcut even inside an input/textarea/select
+ * 
+ * all flags are on by default
+ */
 export function use_bind(shortcut: string, action: (event: KeyboardEvent) => void, options = { prevent_default: true, stop_propagation: true, global: true }) {
   const { prevent_default, stop_propagation, global } = options
 
