3a372151578a5e6984e4586162dcc408c939cabd
Fira <xoyage@gmail.com>
2026-09-07 02:00:01 +0800

    .

--- apps/tools/source/web/component/storybook.tsx
+++ apps/tools/source/web/component/storybook.tsx
@@ -27,8 +27,8 @@ export function Graph() {
   return (
     <svg {...p({ ref: svg, style: {
       backgroundColor: 'khaki'
-    } })}>
-
+    }, viewBox: "0 0 100 100", width: 300, height: 300 })}>
+      <rect x="0" y="0" width="80" height="50" />
     </svg>
   )
 }
