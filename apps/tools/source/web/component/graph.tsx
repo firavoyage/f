8757d84f2a8f76ae8326070e6903a258a06df8fc
Fira <xoyage@gmail.com>
2026-09-08 03:00:04 +0800

    .

--- apps/tools/source/web/component/graph.tsx
+++ apps/tools/source/web/component/graph.tsx
@@ -1,3 +1,5 @@
+import { createContext, useContext } from "react"
+
 const { ceil, floor, abs } = Math
 
 /**
@@ -12,17 +14,21 @@ const { ceil, floor, abs } = Math
  *   - mouse% on the svg
  */
 
+const Coord = createContext()
+
 type graph = {
   aspect_ratio?: number
   padding_left?: number
   padding_bottom?: number
   x: number[]
   y: number[]
+  children?: any
 }
 
 export function Graph(props: graph) {
   const { aspect_ratio = 1, padding_left = 10, padding_bottom = 10,
-    x, y
+    x, y,
+    children
   } = props
 
   const [container, bounds] = use_measure()
@@ -35,17 +41,21 @@ export function Graph(props: graph) {
   const x_begin = x[0]
   const x_end = x[x.length - 1]
   // assume x begin < x end
-  const x_width = x_begin - x_end
+  const x_width = x_end - x_begin
   const y_begin = y[0]
   const y_end = y[y.length - 1]
-  const y_height = y_begin - y_end
+  const y_height = y_end - y_begin
 
   function coordinate_on_viewbox(x: number, y: number) {
+    log({x_begin, x_width, y_begin, y_height})
+
     const x_percentage = (x - x_begin) / x_width
     const x_viewbox = padding_left + graph_width * x_percentage
 
     const y_percentage = (y - y_begin) / y_height
     const y_viewbox = padding_left + graph_width * y_percentage
+
+    log({x_percentage, x_viewbox, y_percentage, y_viewbox})
 
     return { x: x_viewbox, y: y_viewbox }
   }
@@ -65,7 +75,7 @@ export function Graph(props: graph) {
     const y_viewbox = height * y_percentage
     const y_coord = y_viewbox
 
-    return {x: x_coord, y: y_coord}
+    return { x: x_coord, y: y_coord }
   }
 
   useEffect(() => {
@@ -95,17 +105,42 @@ export function Graph(props: graph) {
   })
 
   return (
-    <div className="graph" {...p({ ref: container, style: 'width: 1000px; height: 300px' })}>
-      <svg {...p({
-        style: {
-          width: '100%',
-          height: '100%',
-          backgroundColor: 'khaki'
-        }, viewBox: `0 0 ${width} ${height}`,
-        // preserveAspectRatio: 'none',
-      })}>
-        <rect x="0" y="0" width="80" height="50" />
-      </svg>
-    </div>
+    <Coord value={{ coordinate_on_viewbox, x_begin, x_end, y_begin, y_end }}>
+      <div className="graph" {...p({ ref: container, style: 'width: 1000px; height: 300px' })}>
+        <svg {...p({
+          style: {
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'khaki'
+          }, viewBox: `0 0 ${width} ${height}`,
+          // preserveAspectRatio: 'none',
+        })}>
+          <rect x="0" y="0" width="80" height="50" />
+          {children}
+        </svg>
+      </div>
+    </Coord>
+  )
+}
+
+type line = {
+  line: fn | number
+}
+
+export function Line({ line }: line) {
+  const { coordinate_on_viewbox: coord, x_begin, x_end, y_begin, y_end } = useContext(Coord)
+
+  let s, e
+
+  if (typeof line == 'number') {
+    s = coord(line, y_begin)
+    e = coord(line, y_end)
+  } else {
+    s = coord(x_begin, line(x_begin))
+    e = coord(x_end, line(x_end))
+  }
+
+  return (
+    <line {...p({ x1: s.x, x2: e.x, y1: s.x, y2: e.x })}></line>
   )
 }
