4c37623f6b9e0fb5226ed840da4dc372705513fe
Fira <xoyage@gmail.com>
2026-09-13 05:00:05 +0800

    .

--- apps/tools/source/web/component/popup.tsx
+++ apps/tools/source/web/component/popup.tsx
@@ -1,3 +1,4 @@
+import { tabbable } from "tabbable"
 import { use_bind } from "web/lib/use keyboard"
 
 type popup = {
@@ -21,26 +22,112 @@ export function Popup(props: popup) {
   const popup_ref = useRef()
   const prev_focus_ref = useRef()
 
-  // restore focus when exit
+  // useEffect(() => {
+  //   const handleKeyDown = (e) => {
+  //     if (e.key !== 'Tab') return;
+
+  //     // Cleanly fetches all visible, interactive keyboard-focusable nodes
+  //     const focusable = tabbable(popup_ref.current);
+  //     if (focusable.length === 0) return;
+
+  //     const first_element = focusable[0];
+  //     const last_element = focusable[focusable.length - 1];
+
+  //     if (e.shiftKey && document.activeElement === first_element) {
+  //       last_element.focus();
+  //       e.preventDefault();
+  //     } else if (!e.shiftKey && document.activeElement === last_element) {
+  //       first_element.focus();
+  //       e.preventDefault();
+  //     }
+  //   };
+
+  //   window.addEventListener('keydown', handleKeyDown);
+  //   return () => window.removeEventListener('keydown', handleKeyDown);
+  // }, [open]);
+
+  use_bind('tab', function (e) {
+    if (!popup_ref.current || !open) {
+      return
+    }
+
+    const focusable = tabbable(popup_ref.current);
+    if (focusable.length == 0) {
+      e.preventDefault()
+
+      return
+    };
+
+    const first_element = focusable[0];
+    const last_element = focusable[focusable.length - 1];
+
+    if (document.activeElement == last_element) {
+      first_element.focus()
+      e.preventDefault()
+    }
+  }, {
+    prevent_default: false
+  })
+
+  use_bind('shift+tab', function (e) {
+    if (!popup_ref.current || !open) {
+      return
+    }
+
+    const focusable = tabbable(popup_ref.current);
+    if (focusable.length == 0) {
+      e.preventDefault()
+
+      return
+    };
+
+    const first_element = focusable[0];
+    const last_element = focusable[focusable.length - 1];
+
+    if (document.activeElement == first_element) {
+      last_element.focus()
+      e.preventDefault()
+    }
+  }, {
+    prevent_default: false
+  })
+
+  // focus first element (or popup itself) when open, restore focus when exit
   useEffect(() => {
+    if (!popup_ref.current) {
+      return
+    }
+
     if (open) {
       prev_focus_ref.current = document.activeElement
+
+      // const focusable = tabbable(popup_ref.current);
+      // const first_element = focusable[0];
+      // first_element?.focus()
+      popup_ref.current.focus()
     } else {
       prev_focus_ref.current?.focus()
-    } 
+    }
 
     return () => prev_focus_ref.current?.focus()
   }, [open])
 
+  // trap keyboard when open
+
+
   // listen for outside clicks
   useEvent('mousedown', function (e) {
+    if (backdrop) {
+      // prevent focus loss (backdrop should not get focused)
+      e?.preventDefault()
+    }
+
     if (!popup_ref.current) {
       return
     }
 
     if (!popup_ref.current.contains(e.target)) {
       click_outside()
-      // e.preventDefault()
     }
   })
 
@@ -48,7 +135,7 @@ export function Popup(props: popup) {
     <>
       {
         open &&
-        <div className="popup" {...p({ open, ref: popup_ref })}>
+        <div className="popup" {...p({ open, ref: popup_ref, tabIndex: -1 })}>
           {children}
         </div>
       }
